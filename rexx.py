b97110087e2e7c406b9ce03327a757f73bac6454
Captain-One <pxqjoe@sina.com>
2017-05-17 21:37:06 +0800

    第一次提交

---
+++ rexx.py
@@ -0,0 +1,31 @@
+# 爬虫步骤 两种方法分别介绍
+# 1.使用urllib2 模块 python 3 使用 urllib.Request
+# 2.使用tornado 模块
+
+# urllib.request 主要 Functions:
+# 1.urllib.request.urlopen(url,data=None,context=None)
+# 2.urllib.request.Request(url,data=None,headers={},origin_req_host=None,unverifiable=Flase,method=None)
+
+# tornado.httpclient 主要的类及方法
+# 1.HTTPClient() 类
+# 创建一个http客户端
+# 方法：fetch()
+# 2.HTTPRequest(url,methd="GET",headers=None,connect_timeout=None,request_timeout=None,...) 类
+# 得到一个Request
+
+# 爬虫一般步骤：
+# 1.定义http头
+# 2.定义一个请求，包含url、header等信息
+# 3.根据请求得到响应
+# 4.从响应中提取所需信息
+
+# 示例
+
+import urllib.request
+url = "http://www.youku.com/"
+http_header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0'}
+http_request = urllib.request.Request(url)
+http_response = urllib.request.urlopen(http_request)
+print(http_response.read().decode('utf8'))
+
+# define header
